ecfc7f79e5f8cb15cd9f869628370378cd2d159f
aayush0012 <bhattaayush2006@gmail.com>
2026-07-05 19:55:39 +0530

    added url in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://backend-proj-qphw.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
